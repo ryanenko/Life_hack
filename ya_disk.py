575ba9f18adfb3e4a55a5867fe1209b7f886b062
RYanenko <ryanenko80@gmail.com>
2022-10-22 00:44:54 +0300

    Initial commit

---
+++ ya_disk.py
@@ -0,0 +1,36 @@
+import requests
+from pprint import pprint
+class YandexDisk:
+
+    def __init__(self, token):
+        self.token = token
+
+
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        resalt =  self._get_upload_link(disk_file_path=disk_file_path)
+        href = resalt.get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
